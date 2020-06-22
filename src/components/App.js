81ac2769cebb53eac813b28421c80bc073bc5b35
mattostraszewski <mrostraszewski@gmail.com>
2020-06-22 16:22:03 -0400

    finished axios calls and passing props

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './App.css';
-
+import Post from './Post/Post'
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
 
@@ -10,41 +10,86 @@ class App extends Component {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
+      baseUrl: 'https://swaggerhub.com/apis/DevMountain/social_mountain/1.0.0'
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
-  }
-  
-  componentDidMount() {
-
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
 
-  updatePost() {
-  
+  componentDidMount = () => {
+    const { baseUrl } = this.state
+    axios
+      .get(`${baseUrl}`)
+      .then(res => {
+        this.setState({
+          posts: res.data
+        })
+      })
+      .catch(err => console.log(err))
   }
 
-  deletePost() {
-
+  updatePost(id, text) {
+    const { baseUrl } = this.state
+    axios
+      .put(`${baseUrl}?id=${id}`, text)
+      .then(res => {
+        this.setState({
+          posts: res.data
+        })
+      })
   }
 
-  createPost() {
+  deletePost(id) {
+    const { baseUrl } = this.state
+    axios
+      .delete(`${baseUrl}?id=${id}`)
+      .then(res => {
+        this.setState({
+          posts: res.data
+        })
+      })
+  }
 
+  createPost(text) {
+    const { baseUrl } = this.state
+    axios
+      .post(`${baseUrl}`, { text })
+      .then(res => {
+        this.setState({
+          posts: res.data
+        })
+      })
   }
 
   render() {
     const { posts } = this.state;
-
+    console.log(posts, 'state')
     return (
       <div className="App__parent">
         <Header />
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose
+            createPostFunc={this.createPost}
+          />
+
+          {
+            posts.map(e => {
+              <Post
+                key={e.id}
+                text={e.text}
+                date={e.date}
+                id={e.id}
+                update={this.updatePost}
+                deletePost={this.deletePost}
+              />
+            })
+          }
+
         </section>
       </div>
     );
